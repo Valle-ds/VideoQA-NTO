9dacf71e52860610311eb9577481707625da6464
Valle-ds <83124979+Valle-ds@users.noreply.github.com>
2023-03-01 10:47:50 +0300

    Create __init__.py

---
+++ dataset_prep/__init__.py
@@ -0,0 +1,48 @@
+from activitynet import Activitynetqa
+
+import torch
+from torch.utils.data import DataLoader
+
+def vqa_collate_fn(batch):
+    image_list, question_list, answer_list, weight_list, n = [], [], [], [], []
+    for image, question, answer, weights in batch:
+        image_list.append(image)
+        question_list.append(question)
+        weight_list += weights       
+        answer_list += answer
+        n.append(len(answer))
+    return torch.stack(image_list,dim=0), question_list, answer_list, torch.Tensor(weight_list), n  
+
+def create_dataset(num_retries=1):
+    train_dataset = Activitynetqa('train', num_retries= num_retries, path_to_datadir = 'activitynet/') 
+    test_dataset = Activitynetqa('test', num_retries= num_retries, path_to_datadir = 'activitynet/')
+    return train_dataset, test_dataset
+
+def create_loader(datasets, samplers, batch_size, num_workers, is_trains, collate_fns):
+    loaders = []
+    for dataset,sampler,bs,n_worker,is_train,collate_fn in zip(datasets,samplers,batch_size,num_workers,is_trains,collate_fns):
+        if is_train:
+            shuffle = (sampler is None)
+            drop_last = True
+        else:
+            shuffle = False
+            drop_last = False
+        loader = DataLoader(
+            dataset,
+            batch_size=bs,
+            num_workers=n_worker,
+            pin_memory=True,
+            sampler=sampler,
+            shuffle=shuffle,
+            collate_fn=collate_fn,
+            drop_last=drop_last,
+        )              
+        loaders.append(loader)
+    return loaders    
+
+def create_sampler(datasets, shuffles, num_tasks, global_rank):
+    samplers = []
+    for dataset,shuffle in zip(datasets,shuffles):
+        sampler = torch.utils.data.DistributedSampler(dataset, num_replicas=num_tasks, rank=global_rank, shuffle=shuffle)
+        samplers.append(sampler)
+    return samplers  
